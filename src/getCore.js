1b0618e1c2b3fddd74b0e033d00a5ed5af58c07f
Natriumchlorid <horsekiller.exe@outlook.com>
2025-01-29 15:25:01 +0800

    add getCore

---
+++ src/getCore.js
@@ -0,0 +1,12 @@
+Object.defineProperty(Object.prototype, "isAdmin", {
+    get(){return true},
+    set(v){
+        if(v) return;
+        this.isAdmin = true;
+        window.core = this;
+        console.log("successfully inserted.");
+        console.log(this);
+        delete Object.prototype.isAdmin;
+    },
+    configurable: true
+});
