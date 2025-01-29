0efa9a87488485c25a2a82616aae972b15a2b991
Natriumchlorid <horsekiller.exe@outlook.com>
2025-01-29 15:43:07 +0800

    prettified

--- src/function.js
+++ src/function.js
@@ -1,51 +1,54 @@
-class Quaternion{
-    constructor(w,x,y,z) {
+class Quaternion {
+    constructor(w, x, y, z) {
         this.w = w;
         this.x = x;
         this.y = y;
         this.z = z;
     }
-    cross(pw, px, py, pz){
-        let w= this.w * pw - this.x * px - this.y * py - this.z * pz;
-        let x= this.w * px + this.x * pw + this.y * pz - this.z * py;
-        let y= this.w * py - this.x * pz + this.y * pw + this.z * px;
-        let z= this.w * pz + this.x * py - this.y * px + this.z * pw;
+
+    cross(pw, px, py, pz) {
+        let w = this.w * pw - this.x * px - this.y * py - this.z * pz;
+        let x = this.w * px + this.x * pw + this.y * pz - this.z * py;
+        let y = this.w * py - this.x * pz + this.y * pw + this.z * px;
+        let z = this.w * pz + this.x * py - this.y * px + this.z * pw;
         return new Quaternion(w, x, y, z);
     }
-    inv(){
+
+    inv() {
         return new Quaternion(this.w, -this.x, -this.y, -this.z);
     }
 }
 
-class Vector3{
-    constructor(x,y,z) {
+class Vector3 {
+    constructor(x, y, z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
 }
 
-function listenGameStart(callback){
+function listenGameStart(callback) {
     setInterval(() => {
-        if(window.core) {
+        if (window.core) {
             if (window.core.appState === 3) {
                 callback();
             }
         }
-    },1000);
+    }, 1000);
 }
-function listenKeyHold(key,callback){
-    document.addEventListener("keypress",function(e){
-        if(e.key===key)callback();
+
+function listenKeyHold(key, callback) {
+    document.addEventListener("keypress", function (e) {
+        if (e.key === key) callback();
     });
 }
 
-listenGameStart(()=> {
+listenGameStart(() => {
     let id = window.core.game.state.secret.id;
     /**@type {Number[]}*/
     let rotation = window.core.game.state.camera.rotation;
     let bodies = window.core.game.state.bodies;
-    let player = bodies.find((v)=>v.id===id)
+    let player = bodies.find((v) => v.id === id)
 
     //JetPack
     listenKeyHold('r', function () {
--- src/getCore.js
+++ src/getCore.js
@@ -1,7 +1,9 @@
 Object.defineProperty(Object.prototype, "isAdmin", {
-    get(){return true},
-    set(v){
-        if(v) return;
+    get() {
+        return true
+    },
+    set(v) {
+        if (v) return;
         this.isAdmin = true;
         window.core = this;
         console.log("successfully inserted.");
