d53a2a314c02596229fede3a6470130f8581a432
Natriumchlorid <horsekiller.exe@outlook.com>
2025-01-29 15:24:53 +0800

    listenGameStart

--- src/function.js
+++ src/function.js
@@ -30,18 +30,27 @@ let rotation = window.core.game.state.camera.rotation;
 let bodies = window.core.game.state.bodies;
 let player = bodies.find((v)=>v.id===id)
 
+function listenGameStart(callback){
+    setInterval(() => {
+        if(window.core.appState===3){
+            callback();
+        }
+    },1000);
+}
 function listenKeyHold(key,callback){
     document.addEventListener("keypress",function(e){
         if(e.key===key)callback();
     });
 }
 
+listenGameStart(()=> {
 //JetPack
-listenKeyHold('r',function(){
-    let base = new Quaternion(0,1,0,0);
-    let q = new Quaternion(...rotation);
-    let rq = q.mul(base).mul(base.inv());
-    player.vx = rq.x*2;
-    player.vy = rq.z*2;
-    player.vz = rq.y*2;
+    listenKeyHold('r', function () {
+        let base = new Quaternion(0, 1, 0, 0);
+        let q = new Quaternion(...rotation);
+        let rq = q.mul(base).mul(base.inv());
+        player.vx = rq.x * 2;
+        player.vy = rq.z * 2;
+        player.vz = rq.y * 2;
+    });
 });
